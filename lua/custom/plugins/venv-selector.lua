2fe271483210d1298862d37793290b7256000cb7
raiardinata <raiardinata@gmail.com>
2024-06-26 16:45:24 +0700

    add go lsp configuration

--- lua/custom/plugins/venv-selector.lua
+++ lua/custom/plugins/venv-selector.lua
@@ -3,11 +3,17 @@ return {
     'linux-cultist/venv-selector.nvim',
     dependencies = {
       'neovim/nvim-lspconfig',
-      'nvim-telescope/telescope.nvim',
-      'mfussenegger/nvim-dap-python',
+      'mfussenegger/nvim-dap',
+      'mfussenegger/nvim-dap-python', --optional
+      { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
     },
-    opts = {
-      dap_enabled = true, -- makes the debugger work with venv
+    lazy = false,
+    branch = 'regexp', -- This is the regexp branch, use this for the new version
+    config = function()
+      require('venv-selector').setup()
+    end,
+    keys = {
+      { ',v', '<cmd>VenvSelect<cr>' },
     },
   },
 }
